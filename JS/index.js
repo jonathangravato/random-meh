7d8ff2738c6324a32c66a9fdcedec7d9a96d0885
Point-Break22 <crystalsjgayle@gmail.com>
2022-08-23 16:51:03 -0400

    ugh!

---
+++ JS/index.js
@@ -0,0 +1,12 @@
+
+
+.view {
+    height: 100%;
+    background-color: black no-repeat center;
+    background-size: cover;
+    background-attachment: fixed;
+
+    .view h1 {
+        position: absolute;
+        top: 55%;
+        left: 30%;
